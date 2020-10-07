29992787eeb63aa322ce47fc037a09852cea114c
Jordi Oltra <jordioltra2@gmail.com>
2020-10-08 01:03:15 +0200

    He añadido mensajes para aclarar las funciones

--- web scraping con selenium .py
+++ web scraping con selenium .py
@@ -4,6 +4,8 @@ Created on Wed Oct  7 10:46:59 2020
 
 @author: 20rd1
 """
+
+# Se importan las librerias 
 import random
 import selenium
 from selenium import webdriver
@@ -11,14 +13,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd 
 
+# Ruta para indicar la ubicación del driver necesario para la biblioteca webdriver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+# Le decimos al driver de chrome de que url sacar la información
 driver.get('https://www.olx.com.ec/autos_c378')
 
 
 
-#Vamos a darle al boton 3 veces
+# Buscamos el boton de cargar más 
+# Vamos a darle al boton 3 veces
 
 boton = driver.find_element_by_xpath('//button[@data-aut-id="btnLoadMore"]')
 
@@ -30,19 +35,20 @@ for i in range(3):
     except:
         break
 
-#todos los anuncios de una lista
+# Marcamos donde estan todos los anuncios de una lista
 autos = driver.find_elements_by_xpath('//li[@data-aut-id="itemBox"]')
 
 precio = list()
-descripcion = list ()
+descripcion = list ()               #creamos las listas para almacenar los datos
 
-for auto in autos:
+for auto in autos:                  #creamos un for para meter cada dato en su lista
     precio.append(auto.find_element_by_xpath('.//span[@data-aut-id="itemPrice"]').text)
    
     
     descripcion.append(auto.find_element_by_xpath('.//span[@data-aut-id="itemTitle"]').text)
     
     
+#con la libreria pandas creamos la tabla. 
     
 dict = {'Coches': descripcion,'Precio':precio}
 
